11392e1a04044e907702b91cec452cd0313582ea
ksuvorov <syvorov.kirill@gmail.com>
2026-03-29 00:52:18 +0700

    Adds SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
