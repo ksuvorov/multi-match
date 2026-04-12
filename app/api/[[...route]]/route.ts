2fdea5efe1cc5544d442189d9dad9500392c1ff1
ksuvorov <syvorov.kirill@gmail.com>
2026-04-13 02:41:01 +0700

    Adds listings router & POST / API point

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,8 @@ import { Hono } from 'hono';
 
 import db from '@/lib/db';
 
+import {listingsRouter} from './routers/listings';
+
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
@@ -14,6 +16,8 @@ app.get('/date_db', async (c) => {
     const result = await db.execute(sql`SELECT NOW() as current_time`)
     return c.json({ date: result.rows[0].current_time });
 })
+
+app.route('/listings', listingsRouter);
 
 export const GET = handle(app)
 export const POST = handle(app)
