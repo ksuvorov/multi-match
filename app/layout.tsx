4f2d86d39423b5ce0f004662cc353d7e8216a325
ksuvorov <syvorov.kirill@gmail.com>
2026-04-14 03:43:38 +0700

    Updates layout with cn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
 import type { Metadata, Viewport } from 'next';
 
 import {QueryProvider} from '@/app/providers/query';
+import { cn } from "@/lib/utils";
 
 import AnonymousSessionProvider from './components/anonymousSessionProvider';
 
 import "./globals.css";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={cn(geistSans.variable, geistMono.variable, "font-sans", inter.variable)}>
       <body>
         <QueryProvider>
           <AnonymousSessionProvider />
