64114a39c488b723c30d7444fef4e3241092d72b
ksuvorov <syvorov.kirill@gmail.com>
2026-04-29 15:19:49 +0700

    Updates client auth

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -1,9 +1,9 @@
-'use client'
-
 import { anonymousClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
+import { baseUrl } from '@/lib/baseUrl'
+
 export const authClient = createAuthClient({
-    baseURL: window.location.origin,
+    baseURL: typeof window !== 'undefined' ? window.location.origin : baseUrl,
     plugins: [anonymousClient()],
 })
