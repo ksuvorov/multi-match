e412d2a57a5711df5c819e6aa604a62ab77ec73a
ksuvorov <syvorov.kirill@gmail.com>
2026-05-15 15:53:17 +0700

    Updates Match card

--- app/platform/dive/dashboard/components/Tabs/components/Content/components/MatchCard/index.tsx
+++ app/platform/dive/dashboard/components/Tabs/components/Content/components/MatchCard/index.tsx
@@ -1,13 +1,29 @@
+import {DateTime} from 'luxon';
+
 import {DashboardMatch} from '@/lib/db/queries/match';
+import Period from '@/app/components/Period';
+import {intersection} from '@/lib/date';
 
 import {Card} from '../../../../../Card';
 
+const toDT = (value: string | null) =>
+    value ? DateTime.fromSQL(value, { zone: "utc" }) : null;
+
 export function MatchCard({ match }: { match: DashboardMatch }) {
-    const { counterpart } = match
+    const { counterpart, myListing } = match;
+    const period = intersection(
+        [toDT(counterpart.availableFrom), toDT(counterpart.availableUntil)],
+        [toDT(myListing.availableFrom), toDT(myListing.availableUntil)]
+    );
     return (
         <Card
             title={counterpart.title}
             description={counterpart.description}
+            footer={
+                <>
+                    {period && <Period interval={period} />}
+                </>
+            }
         />
     )
 }
