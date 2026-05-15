f9a63a72b9c6a8745e82bf6841c40a2a27b910f4
ksuvorov <syvorov.kirill@gmail.com>
2026-05-15 15:50:39 +0700

    Adds date library. intersection

---
+++ lib/date.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon';
+
+export type IntervalTuple = [DateTime | null, DateTime | null];
+export const intersection = (
+    [aStart, aEnd]: IntervalTuple,
+    [bStart, bEnd]: IntervalTuple
+): IntervalTuple | null => {
+    const start =
+        aStart && bStart
+            ? aStart > bStart
+                ? aStart
+                : bStart
+            : aStart ?? bStart;
+
+    const end =
+        aEnd && bEnd
+            ? aEnd < bEnd
+                ? aEnd
+                : bEnd
+            : aEnd ?? bEnd;
+
+    if (start && end && start > end) {
+        return null;
+    }
+
+    return [start, end];
+};
